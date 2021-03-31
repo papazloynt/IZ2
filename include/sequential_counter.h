65aee91e134502353c31d4f2d132d394d9d3cd6e
papazloynt <cool.chastikov53@gmail.com>
2021-03-31 22:23:48 +0300

    Sequential counter is work

---
+++ include/sequential_counter.h
@@ -0,0 +1,10 @@
+//
+// Created by chastikov on 31.03.2021.
+//
+
+#ifndef IDZ2_SEQUENTIAL_COUNTER_H
+#define IDZ2_SEQUENTIAL_COUNTER_H
+
+int seq_count(int* arg, const int size);
+
+#endif //IDZ2_SEQUENTIAL_COUNTER_H
