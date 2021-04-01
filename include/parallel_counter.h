8935c64a0e47b34157d1b3d99c56ed82adfbfc13
papazloynt <cool.chastikov53@gmail.com>
2021-04-01 17:43:04 +0300

    Last commit

--- include/parallel_counter.h
+++ include/parallel_counter.h
@@ -5,6 +5,7 @@
 #ifndef IDZ_2_PARALLEL_COUNTER_H
 #define IDZ_2_PARALLEL_COUNTER_H
 
+// Возвращаю только чётные элементы, т.к. нечётные это size - количество чётных
 void* thread_work(void* arg);
 
 int counter (int* arg, const int size);
--- include/sequential_counter.h
+++ include/sequential_counter.h
@@ -4,7 +4,7 @@
 
 #ifndef IDZ2_SEQUENTIAL_COUNTER_H
 #define IDZ2_SEQUENTIAL_COUNTER_H
-
+// Возвращаю только чётные элементы, т.к. нечётные это size - количество чётных
 int counter (int* arg, const int size);
 
 #endif //IDZ2_SEQUENTIAL_COUNTER_H
