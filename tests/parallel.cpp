16dfc54cdff73e0a7cb4ccd2bcaec92e35a2b52a
papazloynt <cool.chastikov53@gmail.com>
2021-04-01 17:18:40 +0300

    add big numbers

--- tests/parallel.cpp
+++ tests/parallel.cpp
@@ -15,6 +15,16 @@ TEST(parallel, true_res) {
     ASSERT_EQ(res, 10);
 }
 
+TEST(parallel, big_numbers) {
+    int size = 100000000;
+    int* p = (int*)malloc(size * sizeof(int));
+    for (int i = 0; i < size; ++i){
+        p[i] = i % 2;
+    }
+    int res = counter(p, size);
+    ASSERT_EQ(res, size / 2);
+}
+
 int main(int argc, char* argv[]){
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
