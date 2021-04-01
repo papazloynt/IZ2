4c4a791ee147203a39e34878ab784188ad753aad
papazloynt <cool.chastikov53@gmail.com>
2021-04-01 16:37:32 +0300

    Gtest is good

--- tests/sequential.cpp
+++ tests/sequential.cpp
@@ -1,4 +1,21 @@
-//
-// Created by chastikov on 01.04.2021.
-//
+#include "gtest/gtest.h"
 
+extern "C" {
+#include "sequential_counter.h"
+#include <malloc.h>
+}
+
+TEST(seq, true_res) {
+    int size = 20;
+    int* p = (int*)malloc(size * sizeof(int));
+    for (int i = 0; i < size; ++i){
+        p[i] = i;
+    }
+    int res = counter(p, size);
+    ASSERT_EQ(res, 10);
+}
+
+int main(int argc, char* argv[]){
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
